4bd1c75c5cbb5a80f6f2c50f18c62d2e80b18352
jack <380225219@qq.com>
2018-09-23 12:17:05 +0800

    add pom for lombok & logback

--- src/main/java/com/ning/blog/controller/IndexController.java
+++ src/main/java/com/ning/blog/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.ning.blog.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/index")
 public class IndexController {
+
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+
     @GetMapping("/test")
     public String index(Model model) {
+        logger.info("进入身体");
         model.addAttribute("username", "fuck");
         return "/index";
     }
