7e55e9d633ae1b8149a145c83e65b8b9fda8b09e
guotongning <guotongning@jrmf360.com>
2018-09-28 14:53:42 +0800

    add something

--- src/main/java/com/ning/blog/controller/IndexController.java
+++ src/main/java/com/ning/blog/controller/IndexController.java
@@ -3,26 +3,44 @@ package com.ning.blog.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/index")
 public class IndexController {
 
     private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
-
-    @GetMapping("/test")
-    public String index(Model model) {
-        logger.info("进入身体");
-        model.addAttribute("username", "fuck");
-        return "/index";
+    @RequestMapping("/share")
+    public String share() {
+        return "share";
+    }
+    @RequestMapping("/list")
+    public String list() {
+        return "list";
+    }
+    @RequestMapping("/life")
+    public String life() {
+        return "life";
+    }
+    @RequestMapping("/time")
+    public String time() {
+        return "time";
+    }
+    @RequestMapping("/gbook")
+    public String gbook() {
+        return "gbook";
+    }
+    @RequestMapping("/info")
+    public String info() {
+        return "info";
+    }
+    @RequestMapping("/about")
+    public String about() {
+        return "about";
+    }
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
     }
 
-    @GetMapping("/login")
-    public String signIn() {
-        return "/login";
-    }
 }
