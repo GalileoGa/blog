78b90cf1072cd04acfa7e2449bf4fbf9575e1850
guotongning <guotongning@jrmf360.com>
2018-10-04 11:35:06 +0800

    Default

--- src/main/java/com/ning/blog/filter/SessionFilter.java
+++ src/main/java/com/ning/blog/filter/SessionFilter.java
@@ -1,5 +1,58 @@
 package com.ning.blog.filter;
 
-public class SessionFilter {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SessionFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //不过滤的url
+        List<String> notFilter = new ArrayList<>();
+        //请求uri
+        String requestURI = request.getRequestURI();
+        //是否过滤
+        boolean doFilter = true;
+        for (String url : notFilter) {//包含则不过滤
+            if (notFilter.indexOf(url) != -1) {
+                doFilter = false;
+                break;
+            }
+        }
+        if (doFilter) {
+            //执行过滤
+            //从Session中过去登陆者实体
+            Object loginUser = request.getSession().getAttribute("loginUser");
+            if (loginUser == null) {
+                boolean isAjaxRequest = isAjaxRequest(request);
+                if (isAjaxRequest) {
+                    response.setCharacterEncoding("UTF-8");
+                    //设置返回错误信息：您已经太长时间没有刷新页面
+                    response.sendError(HttpStatus.UNAUTHORIZED.value(),"您已经太长时间没有刷新页面，请刷新页面。");
+                    return;
+                }
+                response.sendRedirect("");
+                return;
+            }else {
+                //如果已经登陆过了，则继续
+                filterChain.doFilter(request,response);
+            }
+        }
+    }
+
+    private boolean isAjaxRequest(HttpServletRequest request) {
+        String header = request.getHeader("X-Request-With");
+        if (header != null && "XMLHttpRequest".equals(request))
+            return true;
+        else
+            return false;
+    }
 }
